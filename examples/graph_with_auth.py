a0d44fe67ebb565b49f273fb68c921998f9e8190
Sam Partee <Partees21@gmail.com>
2024-10-21 20:19:18 -0700

    Update Authorization example

--- examples/graph_with_auth.py
+++ examples/graph_with_auth.py
@@ -1,3 +1,5 @@
+import time
+
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
@@ -6,12 +8,9 @@ from langgraph.prebuilt import ToolNode
 
 from langchain_arcade import ArcadeToolkit
 
-
-def check_authorize_status(user_id):
-    return user_id == "sam@arcade-ai.com"
-
 # Initialize the Arcade Toolkit with tools
-tools = ArcadeToolkit().get_tools(langgraph=True)
+toolkit = ArcadeToolkit()
+tools = toolkit.get_tools(langgraph=True)
 tool_node = ToolNode(tools)
 
 # Define the model
@@ -28,9 +27,23 @@ def should_continue(state: MessagesState):
     messages = state["messages"]
     last_message = messages[-1]
     if last_message.tool_calls:
-        return "tools"
+        if toolkit.requires_auth(last_message.tool_calls[0]["name"]):
+            return "authorization"
+        else:
+            return "tools"
     return END
 
+def authorize(state: MessagesState, config: dict):
+    user_id = config["configurable"].get("user_id")
+    tool_name = state["messages"][-1].tool_calls[0]["name"]
+    auth_response = toolkit.authorize(tool_name, user_id)
+    if auth_response.status == "completed":
+        return {"messages": state["messages"][-1]}
+    else:
+        print(f"Visit the following URL to authorize: {auth_response.authorization_url}")
+        while not toolkit.is_authorized(auth_response.authorization_id):
+            time.sleep(3)
+        return {"messages": state["messages"][-1]}
 
 # Build the graph
 workflow = StateGraph(MessagesState)
@@ -38,12 +51,12 @@ workflow = StateGraph(MessagesState)
 # Add nodes to the graph
 workflow.add_node("agent", call_agent)
 workflow.add_node("tools", tool_node)
-
+workflow.add_node("authorization", authorize)
 # Define the edges and control flow
 workflow.add_edge(START, "agent")
-workflow.add_conditional_edges("agent", should_continue, ["tools", END])
+workflow.add_conditional_edges("agent", should_continue, ["authorization", "tools", END])
+workflow.add_edge("authorization", "tools")
 workflow.add_edge("tools", "agent")
-
 # Set up memory for checkpointing
 memory = MemorySaver()
 
@@ -63,7 +76,7 @@ inputs = {
 config = {
     "configurable": {
         "thread_id": "4",
-        "user_id": "sam@partee.io"
+        "user_id": "samm@partee.io"
     }
 }
 
--- langchain_arcade/toolkit.py
+++ langchain_arcade/toolkit.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from arcadepy import Arcade
-from arcadepy.types.shared import ToolDefinition
+from arcadepy.types.shared import AuthorizationResponse, ToolDefinition
 from langchain_core.tools import StructuredTool
 
 from langchain_arcade._utilities import (
@@ -32,25 +32,6 @@ class ArcadeToolkit:
         self.client = client or Arcade()
         self._tools = self._retrieve_tool_definitions(tools, toolkits)
 
-    def _retrieve_tool_definitions(
-        self, tools: Optional[list[str]] = None, toolkits: Optional[list[str]] = None
-    ) -> list[ToolDefinition]:
-        tool_definitions = []
-        if tools or toolkits:
-            if tools:
-                tool_definitions.extend(
-                    self.client.tools.definition.get(
-                        director_id="default", tool_id=tool_id
-                    )
-                    for tool_id in tools
-                )
-            if toolkits:
-                for tk in toolkits:
-                    tool_definitions.extend(self.client.tools.list(toolkit=tk))
-        else:
-            # retrieve all tools
-            tool_definitions = self.client.tools.list()
-        return tool_definitions
 
     def get_tools(self, langgraph: bool = False) -> list[StructuredTool]:
         """Get Arcade tools wrapped as LangChain StructuredTool objects.
@@ -71,3 +52,46 @@ class ArcadeToolkit:
                 # Log the error and continue with the next tool
                 print(f"Failed to wrap tool '{tool_def.name}': {e}")
         return lc_tools
+
+    def authorize(self, tool_name: str, user_id: str) -> AuthorizationResponse:
+        """Authorize a user for a tool."""
+        tool_def = self._get_tool_definition(tool_name)
+        toolkit_name = tool_def.toolkit.name
+        full_tool_name = f"{toolkit_name}.{tool_def.name}"
+        return self.client.tools.authorize(tool_name=full_tool_name, user_id=user_id)
+
+    def is_authorized(self, authorization_id: str) -> bool:
+        """Check if a tool authorization is complete."""
+        return self.client.auth.status(authorization_id=authorization_id).status == "completed"
+
+    def requires_auth(self, tool_name: str) -> bool:
+        """Check if a tool requires authorization."""
+
+        tool_def = self._get_tool_definition(tool_name)
+        return tool_def.requirements.authorization is not None
+
+    def _get_tool_definition(self, tool_name: str) -> ToolDefinition:
+        for tool_def in self._tools:
+            if tool_def.name == tool_name:
+                return tool_def
+        raise ValueError(f"Tool '{tool_name}' not found in this ArcadeToolkit instance")
+
+    def _retrieve_tool_definitions(
+        self, tools: Optional[list[str]] = None, toolkits: Optional[list[str]] = None
+    ) -> list[ToolDefinition]:
+        tool_definitions = []
+        if tools or toolkits:
+            if tools:
+                tool_definitions.extend(
+                    self.client.tools.definition.get(
+                        director_id="default", tool_id=tool_id
+                    )
+                    for tool_id in tools
+                )
+            if toolkits:
+                for tk in toolkits:
+                    tool_definitions.extend(self.client.tools.list(toolkit=tk))
+        else:
+            # retrieve all tools
+            tool_definitions = self.client.tools.list()
+        return tool_definitions
